4b121921831733b934f682785c7e52d8d5d07f06
Fluffybook <79397299+Fluffybook@users.noreply.github.com>
2021-08-25 16:23:40 +0400

    edgif

---
+++ public_html/index.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>scripts</title>
+<meta charset="utf-8">
+<meta name="description" content="Scripts roblox">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, bsd, open source, nekotekina, kd11, home">
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/clib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/dualshock3.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3b.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-main-video.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'RKsLVfST-z8',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'_aEVRkxR08k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'8FTQgFT6Znw',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+		<div class="wavebar-con-container-master">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>Discord Gifs<br>
+							 Banners and avatars</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 
+							</p>
+							<div class='landing-btn-container'>
+								<a href="https://rbxscript.com/">
+								<div class='landing-btn-download'>
+									<span>К списку</span>
+								</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(https://i.imgur.com/7bBOA7u.gif) no-repeat center; background-size: contain;">
+					</div>1
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				<div class="search-con-inner">
+					<div class="#">
+						<div "#">
+						
+						</div>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-video darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-btn-left darkmode-arrow-left left" href="#myCarousel" data-slide="prev">
+			</div>
+			<div class="content-btn-right darkmode-arrow-right right" href="#myCarousel" data-slide="next">
+			</div>
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>Featured</h2>
+				</div>
+				<?php include 'lib/module/ui-main-carousel.php';?>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>привет тигр</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 как дела??/
+								</p>
+							</div>
+							<a href="https://github.com/rpcs3" target="_blank">
+							<div class="github-btn-button">
+								<div class="github-ico-button">
+								</div>
+								<div class="github-tx1-button">
+									<span>Join the Project</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="#">
+		<div class='#'>
+			<span></span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="#">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="#">
+					<div class="#">
+						<div class="#">
+						</div>
+					</div>
+					<div class="p#">
+						<div class="#">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									
+								</p>
+							</div>
+							<a href="https://www.patreon.com/Nekotekina" target="_blank">
+							<div class="#">
+								<div class="#">
+								</div>
+								<div class="patreon-tx1-button">
+									<span></span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-discord darkmode-discord">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>The Community</h2>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 We use Discord to house our community. Discord allows us to keep our conversations open and accessible for newcomers seeking various information about the project, how it works and how to get the emulator up and running on your machine.
+								</p>
+							</div>
+							<a href="https://discord.me/rpcs3" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>Join The Discussion</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
